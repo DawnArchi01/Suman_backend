6d5857e5dd88dadafc28dfb868996e09f5b4156b
Archisman Dawn <archismandawn7@gmail.com>
2024-04-30 01:31:07 +0530

    Environment Configured

--- Suman/db/Connection_DB.js
+++ Suman/db/Connection_DB.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
+const URI = "mongodb+srv://Suman:4yP6YbymyIGKkUBE@clustersuman.jfzugcc.mongodb.net/?retryWrites=true&w=majority&appName=ClusterSuman" ;
+
 export const connectDB = async () => {
   try {
-    const { connections } = await mongoose.connect(process.env.MONGO_URI);
+    const { connections } = await mongoose.connect(URI);
     console.log(`DataBase Connected Successfully`);
     // console.log(
     //   `Connection String: ${connections[0]._connectionString}\nHost: ${connections[0].host}, Port: ${connections[0].port} and Database Name: ${connections[0].name}`
--- Suman/index.js
+++ Suman/index.js
@@ -8,7 +8,7 @@ import { connectDB } from "./db/Connection_DB.js";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
 import { notFoundMiddleware } from "./middlewares/notFound.middleware.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000 ;
 
 // // middlewares
 
